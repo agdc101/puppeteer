8720d83a858ece3f01e2302041985f1bd6e5af54
agdc101 <ashleycampbell@MacBook-Pro.local>
2022-12-22 23:29:28 +0000

    text_scraper.js

--- app_screenshot.js
+++ app_screenshot.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-const { isGeneratorObject } = require('util/types');
 
 // runs app
 async function startApp() {
--- text_scrape.js
+++ text_scrape.js
@@ -1,12 +1,20 @@
 const puppeteer = require('puppeteer');
-const { isGeneratorObject } = require('util/types');
+const fs = require('fs/promises');
 
 // runs app
 async function startApp() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://coingecko.com/");
-  await page.screenshot({path: "gecko.png"});
+
+  const coins = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll("td.coin-name span.font-bold")).map(
+      coin => coin.textContent
+      );
+  });
+
+  await fs.writeFile("coin.txt", coins.join("\r"))
+
   await browser.close();
 }
 
