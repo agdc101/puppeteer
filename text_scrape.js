2a7ffc049d5e0168dde8eb132d6f1194fe0b27a6
agdc101 <ashleycampbell@MacBook-Pro.local>
2022-12-22 22:35:13 +0000

    initial commit - screenshot app

---
+++ text_scrape.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+const { isGeneratorObject } = require('util/types');
+
+// runs app
+async function startApp() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://coingecko.com/");
+  await page.screenshot({path: "gecko.png"});
+  await browser.close();
+}
+
+startApp();
