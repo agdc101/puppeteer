78b8e87d32959803d4779d7683688cfc2faea661
agdc101 <agdc101@gmail.com>
2022-12-23 15:13:04 +0000

    text_scraper.js now returns coin list with prices

--- text_scrape.js
+++ text_scrape.js
@@ -3,19 +3,33 @@ const fs = require('fs/promises');
 
 // runs app to scrape text.
 async function startApp() {
-  console.log('hello world');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://coingecko.com/");
 
-  const coins = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("td.coin-name span.font-bold")).map(
+
+  const coinPrices = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".td-price span[data-target='price.price']")).map(
       coin => coin.textContent
-      );
+    );
   });
 
-  await fs.writeFile("coin.txt", coins.join("\r"))
+  const coinNames = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll("td.coin-name span.font-bold")).map(
+      coin => coin.textContent
+    );
+  });
 
+  //combine and arrange arrays into price list.
+  // adjust listLength variable for different list lengths.
+  let x = [];
+  const listLength = 10;
+
+  for (let j = 0; j < listLength; j++) {
+    x += [coinNames[j] + coinPrices[j] + '\n'];
+  }
+
+  await fs.writeFile("coin-prices.txt", x)
   await browser.close();
 }
 
