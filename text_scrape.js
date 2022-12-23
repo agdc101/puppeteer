368fa64c18ae3b2acb08e2529c75182750f658d8
agdc101 <agdc101@gmail.com>
2022-12-23 14:08:21 +0000

    git remote test

--- text_scrape.js
+++ text_scrape.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
 
-// runs app
+// runs app to scrape text.
 async function startApp() {
+  console.log('hello world');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://coingecko.com/");
